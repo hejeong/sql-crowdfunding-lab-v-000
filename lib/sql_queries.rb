9b93823bc0b97223c9d5113598fc237ccf5d7c36
Jonathan Hong <jonhong777@gmail.com>
2018-08-04 03:00:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,11 +20,11 @@ GROUP BY projects.title HAVING amount_over >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, pledges.amount FROM users LEFT OUTER JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY pledges.amount"
+#"SELECT users.name, pledges.amount FROM users LEFT OUTER JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY pledges.amount"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amounts) FROM projects LEFT OUTER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.category"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
